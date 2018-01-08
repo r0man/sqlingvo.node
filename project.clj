af5937d9d7b7f3752a36fa2e3c29d441a032f05e
r0man <roman@burningswell.com>
2018-01-08 16:21:17 +0100

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlingvo.node "0.2.1-SNAPSHOT"
+(defproject sqlingvo.node "0.2.1"
   :description "A ClojureScript driver for SQLingvo on Node.js."
   :url "http://github.com/r0man/sqlingvo.node"
   :license {:name "Eclipse Public License"
